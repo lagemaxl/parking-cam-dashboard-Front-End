2daf9ff1fabe86bee1b1ed3bd123e3996b7e1dbe
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2023-01-06 10:11:53 +0100

    try

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload
         </p>
+        <h1>Ahoj</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
