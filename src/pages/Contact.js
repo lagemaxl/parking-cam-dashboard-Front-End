36ad86ccee98f1dff5cca2cc19549764d4a61ce8
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2023-01-16 11:31:17 +0100

    first build for fun and map

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,22 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 const Contact = () => {
-    return <h1>Contact Me</h1>;
+    return 
+    <div className="App">
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+
+  </div>
   };
   
   export default Contact;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
 import ShowBox from './components/ShowBox';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 
 const data = [
   { name: '12:00', aut: 35},
@@ -18,18 +20,23 @@ const Home = () => {
     return (
       <div className="App">
         <h1 className="Title">Parking Dashboard</h1>
+
         <ShowBox title="Počet míst:" value={90} />
         <ShowBox title="Obsazená místa:" value={10} />
         <ShowBox title="Volná místa:" value={80} />
+
+
+
         <LineChart width={600} height={300} data={data} className="line-chart">
           <Line type="monotone" dataKey="aut" stroke="#8884d8" strokeWidth={3}/>
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip/>
         </LineChart>
+
+
       </div>
     );
-
   };
   
   export default Home;
